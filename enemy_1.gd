5eccb0570096fa81b877ca74456ec7c95a55c207
kosall5220 <kosal.lour@gmail.com>
2026-04-17 09:13:55 -0400

    enemy script and world creation

---
+++ enemy_1.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed = 45
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase:
+		position += (player.position - position)/speed
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+func _on_area_2d_body_exited(body: Node2D):
+	player = null
+	player_chase = false
