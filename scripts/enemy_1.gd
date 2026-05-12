d6d7db1e0f354352666ddc883de09695e49d92cf
kosall5220 <kosal.lour@gmail.com>
2026-05-12 04:41:08 -0400

    edits

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -22,6 +22,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D):
 	player = null
 	player_chase = false
+	
+	
 
 func enemy():
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,7 @@ func _physics_process(delta):
 	player_movement(delta)
 	enemy_attack()
 	attack()
+	update_health()
 	
 	if health <= 0:
 		player_alive = false
@@ -158,3 +159,21 @@ func _on_playerhitbox_area_entered(body: Node2D) -> void:
 func set_count(new_coin_count: int) -> void:
 	coin_counter = new_coin_count
 	coin_label.text = "Score: " + str(coin_counter)
+
+func update_health():
+	var healthbar = $healthbar
+	healthbar.value = health
+	
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+		
+	
+func _on_regin_timer_timeout():
+	if health < 100:
+		health = health + 20
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
